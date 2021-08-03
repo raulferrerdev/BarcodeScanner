2b4c43b767f70f3b26f0876268c65edaa3673ded
RFG <raul.ferrer.dev@gmail.com>
2021-08-03 15:59:01 +0200

    Finishing our Coordinator

--- BarcodeScanner/ScannerView.swift
+++ BarcodeScanner/ScannerView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ScannerView: UIViewControllerRepresentable {
+    
+    @Binding var scannedCode: String
 
     func makeUIViewController(context: Context) -> ScannerVC {
         ScannerVC(scannerDelegate: context.coordinator)
@@ -16,13 +18,20 @@ struct ScannerView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(scannerView: self)
     }
     
     
     final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
         func didFind(barcode: String) {
-            print(barcode)
+            scannerView.scannedCode = barcode
         }
         
         func didSurface(error: CameraError) {
@@ -33,6 +42,6 @@ struct ScannerView: UIViewControllerRepresentable {
 
 struct ScannerView_Previews: PreviewProvider {
     static var previews: some View {
-        ScannerView()
+        ScannerView(scannedCode: .constant("123456"))
     }
 }
